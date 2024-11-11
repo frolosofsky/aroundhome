0f5f0f28692a1893a2934169d4a1add98475cc7d
Stanislav Frolov <frolosofsky@gmail.com>
2024-11-11 01:02:57 +0100

    untabify

--- sql/master.sql
+++ sql/master.sql
@@ -3,17 +3,17 @@ create extension "uuid-ossp";
 
 create table partner(
   id     uuid             not null primary key default gen_random_uuid(),
-	name   text             not null,
-	geo    geography(point) not null,
-	radius float            not null,
-	rating int              not null
+  name   text             not null,
+  geo    geography(point) not null,
+  radius float            not null,
+  rating int              not null
 );
 create index on partner using gist (geo);
 
 create table partner_skill(
-	partner_id uuid not null references partner(id) on update restrict on delete restrict,
-	code       text not null,
-	unique(partner_id, code)
+  partner_id uuid not null references partner(id) on update restrict on delete restrict,
+  code       text not null,
+  unique(partner_id, code)
 );
 
 create index on partner_skill(code);
